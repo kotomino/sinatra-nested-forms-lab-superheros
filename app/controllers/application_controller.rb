13a0baf151696d4ad96d671531c60bcf460fdf30
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2020-12-24 11:21:42 -0600

    commit tree 9da9da906ae232aeaa4a5cfca459305d3d16624e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,24 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/teams' do
+        @team_name = params[:team][:name]
+        @team_motto = params[:team][:motto]
+        @heros = params[:team][:heros]
+        @hero_names = []
+        @hero_powers = []
+        @hero_bios = []
+
+        @heros.each do |hero|
+            @hero_names << hero[:name]
+            @hero_powers << hero[:power]
+            @hero_bios << hero[:bio]
+        end
+
+        erb :team
+    end
 end
